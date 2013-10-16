ce6f4299d3fc4438fa6598fb4fdd010889619aae
Jiri Napravnik <jiri.napravnik@gmail.com>
2013-10-16 23:48:40 +0200

    fix extends

--- src/JiriNapravnik/Application/UI/Presenter.php
+++ src/JiriNapravnik/Application/UI/Presenter.php
@@ -2,12 +2,14 @@
 
 namespace JiriNapravnik\Application\UI;
 
+use Nette;
+
 /**
  * Base Presenter
  * @author Jiří Nápravník (http://jirinapravnik.cz)
  * @copyright Copyright (c) 2013, Jiří Nápravník
  */
-class Presenter extends Presenter
+class Presenter extends Nette\Application\UI\Presenter
 {
 
 	use \Nextras\Application\UI\SecuredLinksPresenterTrait;
