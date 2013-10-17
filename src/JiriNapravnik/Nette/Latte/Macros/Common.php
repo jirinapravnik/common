7a6d4ceec309c323cff2f454ef6628a48ae0b7d5
Jiri Napravnik <jiri.napravnik@gmail.com>
2013-10-17 19:45:43 +0200

    latte macros

--- src/JiriNapravnik/Nette/Latte/Macros/Common.php
+++ src/JiriNapravnik/Nette/Latte/Macros/Common.php
@@ -1,26 +1,33 @@
 <?php
 
-namespace Enbros\Nette\Latte\Macros;
+namespace JiriNapravnik\Nette\Latte\Macros;
 
-use \Nette\Latte\MacroNode;
-use \Nette\Latte\PhpWriter;
+use Nette\Latte\Compiler;
+use Nette\Latte\MacroNode;
+use Nette\Latte\Macros\MacroSet;
+use Nette\Latte\PhpWriter;
 
-class Common extends \Nette\Latte\Macros\MacroSet
+/*
+ * Macros for latte
+ * @author Jiří Nápravník (http://jirinapravnik.cz)
+ * @copyright Copyright (c) 2013, Jiří Nápravník
+ */
+
+class Common extends MacroSet
 {
 
-    public static function install(\Nette\Latte\Compiler $compiler)
-    {
-        $set = new static($compiler);
-        $set->addMacro('confirm', NULL, NULL, array($set, 'macroConfirm'));
-    }
+	public static function install(Compiler $compiler)
+	{
+		$set = new static($compiler);
+		$set->addMacro('confirm', NULL, NULL, array($set, 'macroConfirm'));
+	}
 
-    /**
-     * n:confirm="..."
-     */
-    public function macroConfirm(MacroNode $node, PhpWriter $writer)
-    {
-        return $writer->write('echo \'data-confirm="\' . %escape("' . $node->args . '") . \'"\' ');
-    }
+	/**
+	 * n:confirm="..."
+	 */
+	public function macroConfirm(MacroNode $node, PhpWriter $writer)
+	{
+		return $writer->write('echo \'data-confirm="\' . %escape("' . $node->args . '") . \'"\' ');
+	}
 
 }
-
