160c849759e49320f87134db52822ee8051e8698
Jiri Napravnik <jiri.napravnik@gmail.com>
2013-11-18 15:26:56 +0100

    sequence listeners + TablePrefixSubscriber.php

---
+++ src/JiriNapravnik/Doctrine/EntityListener/RootSequenceListener.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Listener for page entity
+ *
+ * @author Jiří Nápravník (jirinapravnik.cz), Enbros.cz
+ */
+
+namespace JiriNapravnik\Doctrine\EntityListener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Mapping as ORM;
+
+class RootSequenceListener
+{
+
+	/**
+	 * @ORM\PrePersist 
+	 */
+	public function prePersist($item, LifecycleEventArgs $event)
+	{
+		$this->setMaxRootSequence($item, $event);
+	}
+
+	/**
+	 * @ORM\PostRemove 
+	 */
+	public function postRemove($item, LifecycleEventArgs $event)
+	{
+		if ($item->getLevel() > 0) {
+			return;
+		}
+
+		$em = $event->getEntityManager();
+
+		$qb = $em->createQueryBuilder();
+		$qb->update(get_class($item), 'i')
+			->set('i.rootSequence', 'i.rootSequence - 1')
+			->where('i.rootSequence > ' . $item->getRootSequence())
+			->getQuery()
+			->execute();
+	}
+
+	/**
+	 * @ORM\PreUpdate
+	 */
+	public function preUpdate(\CategoryModule\Model\Entity\Category $item, PreUpdateEventArgs $event)
+	{
+		if ($item->getLevel() > 0) {
+			$item->setRootSequence(NULL);
+		} elseif ($event->hasChangedField('parent') && is_null($event->getNewValue('parent')) && !is_null($event->getOldValue('parent'))) {
+			$this->setMaxRootSequence($item, $event);
+		} else {
+			$oldSequence = $event->getOldValue('rootSequence');
+			$newSequence = $event->getNewValue('rootSequence');
+
+			if ($oldSequence === $newSequence) {
+				return;
+			}
+
+			$em = $event->getEntityManager();
+			
+			$qb = $em->createQueryBuilder();
+			$qb->update(get_class($item), 'i')
+				->where('i.rootSequence = ' . $newSequence);
+
+			if ($newSequence > $oldSequence) {
+				$qb->set('i.rootSequence', 'i.rootSequence - 1');
+			} elseif ($newSequence < $oldSequence) {
+				$qb->set('i.rootSequence', 'i.rootSequence + 1');
+			}
+
+			$qb->getQuery()
+				->execute();
+		}
+	}
+
+	private function setMaxRootSequence($item, LifeCycleEventArgs $event)
+	{
+		$em = $event->getEntityManager();
+		
+		$qb = $em->createQueryBuilder();
+		$maxSequence = $qb->select('MAX(i.rootSequence)')
+			->from(get_class($item), 'i')
+			->getQuery()
+			->getOneOrNullResult();
+
+		$sequenceNext = (int) $maxSequence[1] + 1;
+
+		if (is_null($item->getParent())) {
+			$item->setRootSequence($sequenceNext);
+		}
+	}
+
+}
--- src/JiriNapravnik/Nette/BaseForm.php
+++ src/JiriNapravnik/Nette/BaseForm.php
@@ -13,11 +13,10 @@ namespace JiriNapravnik\Nette;
 class BaseForm extends \Nette\Application\UI\Form
 {
 
-    public function __construct(\Nette\ComponentModel\IContainer $parent = NULL, $name = NULL)
-    {
-        parent::__construct($parent, $name);
-        $this->setRenderer(new \Kdyby\BootstrapFormRenderer\BootstrapRenderer());
-    }
+	public function __construct(\Nette\ComponentModel\IContainer $parent = NULL, $name = NULL)
+	{
+		parent::__construct($parent, $name);
+		$this->setRenderer(new \Kdyby\BootstrapFormRenderer\BootstrapRenderer());
+	}
 
 }
-
