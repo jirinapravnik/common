574f18b991673cd55e870dd915752d70ce6718f7
Jiri Napravnik <jiri.napravnik@gmail.com>
2013-12-28 15:49:25 +0100

    exceptions

---
+++ src/exceptions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * exceptions
+ *
+ * @author Jiří Nápravník (http://jirinapravnik.cz)
+ * @copyright Copyright (c) 2013, Jiří Nápravník
+ */
+class NotImplementedException extends \LogicException
+{
+	
+}
+
+class RecordNotFoundException extends \RuntimeException
+{
+	
+}
+
+class InvalidStateException extends \RuntimeException
+{
+	
+}
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+	
+}
