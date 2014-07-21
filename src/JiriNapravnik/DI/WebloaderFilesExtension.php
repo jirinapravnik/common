1fc89f7f019ca8411b754c1b09c7b49d5db1d515
Jiří Nápravník <jiri.napravnik@gmail.com>
2014-07-21 22:24:53 +0200

    rename variables - conflicts

--- src/JiriNapravnik/DI/WebloaderFilesExtension.php
+++ src/JiriNapravnik/DI/WebloaderFilesExtension.php
@@ -17,11 +17,11 @@ class WebloaderFilesExtension extends \Nette\DI\CompilerExtension
 		$webloader = $this->compiler->getExtensions('\WebLoader\Nette\Extension')['webloader'];
 		foreach ($this->compiler->getExtensions() as $extension) {
 			if ($extension instanceof \JiriNapravnik\DI\IWebloaderFilesProvider) {
-				$files = $extension->getWebloaderFiles();
-				foreach (['css', 'js'] as $type) {
-					if (isset($files[$type])) {
-						foreach ($files[$type] as $name => $files) {
-							$filesDef = $this->getContainerBuilder()->getDefinition($webloader->prefix($type . ucfirst($name) . 'Files'));
+				$assets = $extension->getWebloaderFiles();
+				foreach (['css', 'js'] as $assetsType) {
+					if (isset($assets[$assetsType])) {
+						foreach ($assets[$assetsType] as $name => $files) {
+							$filesDef = $this->getContainerBuilder()->getDefinition($webloader->prefix($assetsType . ucfirst($name) . 'Files'));
 							foreach ($files as $file) {
 								$filesDef->addSetup('addFile', [$file]);
 							}
