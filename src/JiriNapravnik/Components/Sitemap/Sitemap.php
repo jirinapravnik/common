553057850624cd7e4b7e1285555bc15c695fd334
Jiri Napravnik <jiri.napravnik@gmail.com>
2014-10-29 12:51:09 +0100

    sitemap

---
+++ src/JiriNapravnik/Components/Sitemap/Sitemap.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace JiriNapravnik\Components\Sitemap;
+
+use DateTime;
+use Nette\Application\UI\Control;
+use Nette\Latte\Engine;
+use Nette\Templating\FileTemplate;
+
+class Sitemap extends Control
+{
+
+	const MAX_URL = 10000;
+
+	private $urlToSitemap;
+	private $sitemapDir;
+	private $subdomain;
+	private $urlsToSitemap = [];
+	private $sitemapForSitemapIndex = [];
+
+	public function __construct($urlToSitemap, $sitemapDir, $subdomain = NULL)
+	{
+		$this->urlToSitemap = $urlToSitemap;
+		$this->sitemapDir = $sitemapDir;
+		$this->subdomain = $subdomain;
+	}
+
+	public function generateSitemap($items)
+	{
+		$i = 0;
+		$sitemapId = 1;
+		foreach ($items as $item) {
+			$this->addUrlToSitemap($item['loc'], $item['lastmod'], $item['changefreq'], $item['priority']);
+			$i++;
+			if ($i == self::MAX_URL) {
+				$this->saveSitemap($sitemapId);
+				$i = 0;
+				$sitemapId++;
+			}
+		}
+		if (count($this->urlsToSitemap) > 0) {
+			$this->saveSitemap($sitemapId);
+		}
+
+		$this->saveSitemapIndex();
+	}
+
+	private function saveSitemap($sitemapId)
+	{
+		$sitemapTemplate = new FileTemplate(__DIR__ . '/sitemap.latte');
+		$sitemapTemplate->registerFilter(new Engine);
+		$sitemapTemplate->registerHelperLoader('\Nette\Templating\Helpers::loader');
+		$sitemapTemplate->urlsToSitemap = $this->urlsToSitemap;
+		$xmlToSave = $sitemapTemplate->__toString();
+
+		if($this->subdomain !== NULL){
+			$filename = 'sitemap-' . $this->subdomain . '-' . $sitemapId . '.xml';
+		} else {
+			$filename = 'sitemap-' . $sitemapId . '.xml';
+		}
+
+
+		$fullFilename = $this->sitemapDir . $filename;
+		if (file_exists($fullFilename)) {
+			unlink($fullFilename);
+		}
+
+		file_put_contents($fullFilename, $xmlToSave);
+		$this->urlsToSitemap = [];
+		$this->sitemapForSitemapIndex[] = [
+			'loc' => $this->urlToSitemap . $filename,
+			'lastmod' => $this->getCorrectDate(filemtime($fullFilename)),
+		];
+	}
+
+	private function saveSitemapIndex()
+	{
+		$sitemapIndexTemplate = new FileTemplate(__DIR__ . '/sitemapIndex.latte');
+		$sitemapIndexTemplate->registerFilter(new Engine);
+		$sitemapIndexTemplate->registerHelperLoader('\Nette\Templating\Helpers::loader');
+		$sitemapIndexTemplate->sitemapForSitemapIndex = $this->sitemapForSitemapIndex;
+		$xmlToSave = $sitemapIndexTemplate->__toString();
+
+		if($this->subdomain !== NULL){
+			$filename = 'sitemapIndex-' . $this->subdomain . '.xml';
+		} else {
+			$filename = 'sitemapIndex.xml';
+		}
+		
+		if (file_exists($filename)) {
+			unlink($filename);
+		}
+
+		file_put_contents($this->sitemapDir . $filename, $xmlToSave);
+	}
+
+	private function addUrlToSitemap($loc, $lastmod, $changefreq, $priority)
+	{
+		$this->urlsToSitemap[] = [
+			'loc' => $loc,
+			'lastmod' => $this->getCorrectDate($lastmod),
+			'changefreq' => $changefreq,
+			'priority' => $priority,
+		];
+	}
+
+	private function getCorrectDate($datetime)
+	{
+		if(is_string($datetime)){
+			$datetime = strtotime($datetime);
+		}
+		if (!$datetime instanceof DateTime) {
+			$timestamp = $datetime;
+			$datetime = new DateTime;
+			$datetime->setTimestamp($timestamp);
+		}
+		return $datetime->format('Y-m-d');
+	}
+
+}
