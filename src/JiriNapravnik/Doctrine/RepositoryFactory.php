8989de9db6e4b315f0efb09a9bf8253b39b686be
Jiří Nápravník <jiri.napravnik@gmail.com>
2015-12-18 00:39:07 +0100

    entityrepositoryfactory

---
+++ src/JiriNapravnik/Doctrine/RepositoryFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JiriNapravnik\Doctrine;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Tree\TreeListener;
+use JiriNapravnik\Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use Kdyby\Doctrine\RepositoryFactory as KdybyRepositoryFactory;
+use Nette\DI\Container;
+
+class RepositoryFactory extends KdybyRepositoryFactory
+{
+	private $serviceLocator;
+	
+	public function __construct(Container $serviceLocator)
+	{
+		parent::__construct($serviceLocator);
+		$this->serviceLocator = $serviceLocator;
+	}
+	
+	public function getRepository(EntityManagerInterface $entityManager, $entityName)
+	{
+		$repository = parent::getRepository($entityManager, $entityName);
+		if($repository instanceof NestedTreeRepository){
+			$repository->setTreeListener($this->serviceLocator->getByType(TreeListener::class));
+		}
+		
+		return $repository;
+	}
+
+}
