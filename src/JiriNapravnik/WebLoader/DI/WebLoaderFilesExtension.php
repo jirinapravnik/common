ff84648f0eeba3f124bda58888dd82027604f4ca
Jiri Napravnik <jiri.napravnik@gmail.com>
2016-12-03 01:12:29 +0100

    Webloader -> WebLoader

--- src/JiriNapravnik/WebLoader/DI/WebLoaderFilesExtension.php
+++ src/JiriNapravnik/WebLoader/DI/WebLoaderFilesExtension.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 /**
  * WebloaderExtension
@@ -16,7 +16,7 @@ class WebLoaderFilesExtension extends \Nette\DI\CompilerExtension
 	{
 		$webloader = $this->compiler->getExtensions('\WebLoader\Nette\Extension')['webloader'];
 		foreach ($this->compiler->getExtensions() as $extension) {
-			if ($extension instanceof \JiriNapravnik\DI\IWebloaderFilesProvider) {
+			if ($extension instanceof \JiriNapravnik\WebLoader\DI\IWebloaderFilesProvider) {
 				$assets = $extension->getWebloaderFiles();
 				foreach (['css', 'js'] as $assetsType) {
 					if (isset($assets[$assetsType])) {
